ffdba16ef234d79c52263c7e02d6f709ae55fec9
VeronikaPolkhanova <halky.boom>
2022-02-22 19:09:07 +0300

    added datapicker

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ const USD = 2.60;
 const EUR = 2.94;
 const CNY = 0.41;
 
-function CheckValue(value) {
-    switch (value) {
+function Rand(value) {
+    let rand = Math.random();
+    if (rand > 0.5) return (value - value * (rand / 10)).toFixed(2);
+    else return (value + value * (rand / 10)).toFixed(2);
+}
+
+function Convert(from_currency, to_currency, value) {
+    return ((from_currency / to_currency) * value).toFixed(2);
+}
+
+function CheckSelect(select) {
+    switch (select) {
         case "BYN":
             return BYN;
         case "RUB":
@@ -19,36 +29,78 @@ function CheckValue(value) {
     }
 }
 
-function Convert(from_curency, to_curency, value) {
-    if (from_curency === to_curency)
-        return value
-    else if (from_curency === BYN)
-        return value / to_curency
-    else if (to_curency === BYN)
-        return value * from_curency
-    else
-        return (value * from_curency) / to_curency
+function CheckValue(input_value) {
+    if (input_value < 0) {
+        alert("Количество валюты не может быть меньше нуля!");
+        document.getElementById('inputValue').value = Math.abs(input_value);
+        return Math.abs(input_value);
+    }
+    else return input_value
 }
 
-let selected_from = document.getElementById('from_curency_select').value;
-
-function onFromSelect() {
-    selected_from = document.getElementById('from_curency_select').value;
+function CheckDate(date) {
+    const new_date = new Date(date);
+    const today = new Date();
+    if (new_date > today) {
+        alert("Нельзя посмотреть курс на будущее");
+        document.getElementById('data_picker').value = today.format("yyyy-mm-dd");
+        return today.format("yyyy-mm-dd")
+    }
+    else return date
 }
 
-function onToSelect() {
-    selected_to = document.getElementById('to_curency_select').value;
+let selected_from = document.getElementById('from_currency_select').value;;
+let data_value;
+const min = new Date();
+min.setDate(min.getDate() - 14);
+
+document.getElementById('data_picker').value = (new Date()).format("yyyy-mm-dd");
+document.getElementById('data_picker').min = min.format("yyyy-mm-dd");
+
+document.getElementById('currency_BYN').innerHTML = BYN;
+document.getElementById('currency_RUB').innerHTML = RUB;
+document.getElementById('currency_EUR').innerHTML = EUR;
+document.getElementById('currency_USD').innerHTML = USD;
+document.getElementById('currency_CNY').innerHTML = CNY;
+
+function onDataSelect() {
+    data_value = document.getElementById('data_picker').value;
+    data_value = CheckDate(data_value);
+
+    const new_date = new Date(data_value).getDate();
+    const today = new Date().getDate();
+
+    if (new_date === today) {
+        document.getElementById('currency_BYN').innerHTML = BYN;
+        document.getElementById('currency_RUB').innerHTML = RUB;
+        document.getElementById('currency_EUR').innerHTML = EUR;
+        document.getElementById('currency_USD').innerHTML = USD;
+        document.getElementById('currency_CNY').innerHTML = CNY;
+    }
+    else if (today > new_date) {
+        document.getElementById('currency_BYN').innerHTML = Rand(BYN);
+        document.getElementById('currency_RUB').innerHTML = Rand(RUB);
+        document.getElementById('currency_EUR').innerHTML = Rand(EUR);
+        document.getElementById('currency_USD').innerHTML = Rand(USD);
+        document.getElementById('currency_CNY').innerHTML = Rand(CNY);
+    }
 }
 
-function res(event) {
-    from_curency = CheckValue(selected_from);
-    let input_value = Number(document.getElementById('inputValue').value);
+function onCurrencySelect() {
+    selected_from = document.getElementById('from_currency_select').value;
+}
 
-    const output_BYN = Convert(from_curency, BYN, input_value).toFixed(2);
-    const output_RUB = Convert(from_curency, RUB, input_value).toFixed(2);
-    const output_EUR = Convert(from_curency, EUR, input_value).toFixed(2);
-    const output_USD = Convert(from_curency, USD, input_value).toFixed(2);
-    const output_CNY = Convert(from_curency, CNY, input_value).toFixed(2);      
+function onInput(event) {
+    let input_value = Number(document.getElementById('input_value').value);
+
+    input_value = CheckValue(input_value);
+    from_currency = CheckSelect(selected_from);
+
+    const output_BYN = Convert(from_currency, BYN, input_value);
+    const output_RUB = Convert(from_currency, RUB, input_value);
+    const output_EUR = Convert(from_currency, EUR, input_value);
+    const output_USD = Convert(from_currency, USD, input_value);
+    const output_CNY = Convert(from_currency, CNY, input_value);
 
     document.getElementById('output_BYN').innerHTML = `${output_BYN} Br`;
     document.getElementById('output_RUB').innerHTML = `${output_RUB} ₽`;
